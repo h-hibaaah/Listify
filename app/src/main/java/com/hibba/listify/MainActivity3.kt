be8919cb1f25ffc25e1a21dbd1d4e14cd3e993a0
h-hibaaah <hibba5825@gmail.com>
2026-09-11 19:36:20 +0500

    Kotlin Added

--- app/src/main/java/com/hibba/listify/MainActivity3.kt
+++ app/src/main/java/com/hibba/listify/MainActivity3.kt
@@ -5,13 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
-        val bott
+
+        val bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        if (savedInstanceState == null)
+        {
+            val home_frag = Home()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment,home_frag)
+                .commit()
+        }
+        bottomnav.setOnItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment, Home())
+                        .commit()
+                    true
+
+                }
+//
+//                R.id.lists -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.fragment, Lists())
+//                        .commit()
+//                    true
+//                }
+//
+//                R.id.profile -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.fragment, Profile())
+//                        .commit()
+//                    true
+//                }
+
+                else -> false
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
