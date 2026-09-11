da77206d297945874db6488399f7061cf557c264
h-hibaaah <hibba5825@gmail.com>
2026-09-11 19:16:59 +0500

    Design of activity3 completed

--- app/src/main/java/com/hibba/listify/MainActivity3.kt
+++ app/src/main/java/com/hibba/listify/MainActivity3.kt
@@ -11,6 +11,7 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
+        val bott
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
