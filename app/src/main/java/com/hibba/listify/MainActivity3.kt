ebe944a9ee97adbb73ccafcb66353bdce9442a1d
h-hibaaah <hibba5825@gmail.com>
2026-09-11 12:38:14 +0500

    MEnu Resource directory for bottom navigation added

--- app/src/main/java/com/hibba/listify/MainActivity3.kt
+++ app/src/main/java/com/hibba/listify/MainActivity3.kt
@@ -1,25 +1,16 @@
 package com.hibba.listify
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val button = findViewById<AppCompatButton>(R.id.appCompatButton)
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-        }
-
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
