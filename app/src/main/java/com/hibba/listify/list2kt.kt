6d7db4c947e7e2a3692050b85b0e9c9334c82276
h-hibaaah <hibba5825@gmail.com>
2026-09-22 20:07:54 +0500

    ViewHolder Function Completed

--- app/src/main/java/com/hibba/listify/list2kt.kt
+++ app/src/main/java/com/hibba/listify/list2kt.kt
@@ -2,6 +2,7 @@ package com.hibba.listify
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class listrecyclerview(val items : List<list1kt>) : RecyclerView.Adapter<listrecyclerview.viewholderlist>()
@@ -23,6 +24,7 @@ class listrecyclerview(val items : List<list1kt>) : RecyclerView.Adapter<listrec
 
     class viewholderlist (itemview1 : View): RecyclerView.ViewHolder(itemview1)
     {
-
+        val text1 = itemview1.findViewById<TextView>(R.id.text1)
+        val text2 = itemview1.findViewById<TextView>(R.id.text2)
     }
 }
