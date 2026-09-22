2369be107b5b3e4174f7de4fd1cefbe601370ed8
h-hibaaah <hibba5825@gmail.com>
2026-09-22 20:01:34 +0500

    two kt files created

---
+++ app/src/main/java/com/hibba/listify/list1kt.kt
@@ -0,0 +1,4 @@
+package com.hibba.listify
+
+data class list1kt (
+    val text1: String)
---
+++ app/src/main/java/com/hibba/listify/list2kt.kt
@@ -0,0 +1,28 @@
+package com.hibba.listify
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class listrecyclerview(val items : List<list1kt>) : RecyclerView.Adapter<listrecyclerview.viewholderlist>()
+{
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): listrecyclerview.viewholderlist {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: listrecyclerview.viewholderlist, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class viewholderlist (itemview1 : View): RecyclerView.ViewHolder(itemview1)
+    {
+
+    }
+}
